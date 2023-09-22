e6f5b19c0366f5d50a0196f0eaf75f9bbf6c111f
denisostapenko <denys.ostapenko@auk.edu.ua>
2023-09-22 13:30:36 +0300

    )

--- src/Homework/Recursion.java
+++ src/Homework/Recursion.java
@@ -1,2 +1,50 @@
-package Homework;public class Recursion {
+package Homework;
+
+public class Recursion {
+    public static void main(String[] args) {
+        printNumberBackward(9);
+        System.out.println();
+        printNumber(9);
+        //            stackOverFlow(1);
+        System.out.println();
+        int[] array = {1, 2, 3, 4, 5};
+        printArray(array, 0);
+        System.out.println();
+        int sum = arraySum(array, 0);
+        System.out.println(sum);
+    }
+
+    public static void printNumberBackward(int n){
+        if (n > 0){
+            System.out.print(n + " ");
+            n--;
+            printNumberBackward(n);
+        }
+    }
+
+    public static void printNumber(int n){
+        if (n > 0){
+            printNumber(n - 1);
+            System.out.print(n + " ");
+        }
+    }
+
+//    public static void stackOverFlow(int n){
+//        System.out.println(n);
+//        stackOverFlow(n);
+//    }
+
+    public static void printArray(int[] array, int i){
+        if (array.length == i){
+            return;
+        }
+        System.out.print(array[i] + " ");
+        printArray(array, i+1);
+    }
+    public static int arraySum(int[] array, int i){
+        if (array.length == i){
+            return 0;
+        }
+        return array[i] + arraySum(array, i + 1);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,46 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
+    static int i;
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        printNumbersUp(1);
+    }
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static void explosion( int n) {
+        if (n == 0) {
+            System.out.println("BAM!!!");
+            return;
+        }
+        System.out.println(n);
+        explosion(n-1);
+    }
+    public static void func(){
+        i++;
+        System.out.println("Call number:" + i + ".");
+        // BASE CASE
+        if (i < 100)
+            func();
+    }
+    public static int factorial (int num){
+        if (num == 0) return 1;
+        if (num == 1) return 1;
+        return num * factorial(num - 1);
+    }
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+    public static void printNumbersUp(int i) {
+        if (i < 6) {
+            System.out.println(i);
+            i++;
+            printNumbersUp(i);
+        }else {
+            System.out.println("End");
         }
     }
+
+    /** main
+     * printNumbersUp(1)
+     * printNumbersUp(2)
+     * printNumbersUp(3)
+     * printNumbersUp(4)
+     * printNumbersUp(5)
+     */
 }
